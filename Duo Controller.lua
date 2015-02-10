413992165f5e7a0155472e56cf791955ba994205
thelaw44 <ahmetcanguven44@gmail.com>
2015-02-10 08:22:10 +0200

    Update Duo Controller.lua

--- Duo Controller.lua
+++ Duo Controller.lua
@@ -1,7 +1,7 @@
 local socket = require("socket")
 local tcp = socket.tcp()
 tcp:settimeout(10)
-local ok,err = tcp:connect("localhost", 5000)
+local ok,err = tcp:connect("127.0.0.1", 5000)
 if not ok then
    print('Could not connect Error: ',err)
    return
@@ -11,17 +11,34 @@ end
 --Connection Done
 
 --Tcp Send
-tcp:send("hello world");
 
 --Tcp Recieve
 lastTick = 0
+
+function OnLoad()
+	tcp:send("setup,"..createGameId()..","..myHero.charName);
+
+end
+
 function OnTick()
 	tcpListen()
 end
 
 function tcpListen()
+	tcp:settimeout(0)
 	local s, status, partial = tcp:receive()
-	if #partial > 1 then
-		print(partial)
+	print(partial)
+	--if #partial > 1 then
+	--	print(partial)
+	--end
+end
+
+function createGameId()
+	enemyHeroes = GetEnemyHeroes()
+	allyHeroes = GetStart()
+	local id = ""
+	for _, ally in pairs(allyHeroes) do
+		print(ally)
 	end
+	return id
 end
